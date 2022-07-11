4ff781894a3f875ad2a946895677380266fad38d
林 岳斗 <gakuto404@gmail.com>
2022-07-11 16:57:52 +0900

    [add]type&list

---
+++ list.py
@@ -0,0 +1,7 @@
+names = ["佐藤", "田中", "鈴木"]
+
+names[1] = "坂本"
+
+print(names[0])
+print(names[1])
+print(names[2])
---
+++ type.py
@@ -0,0 +1,20 @@
+num01 = 1
+num02 = 1.23
+
+string_a = "Hello,World!"
+
+a = 10
+b = 1
+
+bool01 = (a > b)
+bool02 = (a < b)
+
+print(type(num01))
+print(type(num02))
+
+print(string_a)
+print(type(string_a))
+
+print(bool01)
+print(bool02)
+print(type(bool01))
